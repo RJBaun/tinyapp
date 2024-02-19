f077c63bc95683e316e202e2d41d1adb7506ed1a
Rylan Baun <rylanjbaun@gmail.com>
2024-02-19 17:34:18 -0600

    Adding endpoint for rendering urlDatabase with ejs

--- express_server.js
+++ express_server.js
@@ -21,6 +21,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const urlVars = {urls: urlDatabase}
+  res.render("ulrs_index", urlVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
